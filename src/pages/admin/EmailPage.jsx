183c01ee204e8398aba0b607397df24a699005d7
alessandroparrelli <alessandroparrelli@gmail.com>
2026-08-28 14:16:45 +0200

    Aggiornamento EmailPage.jsx

--- src/pages/admin/EmailPage.jsx
+++ src/pages/admin/EmailPage.jsx
@@ -42,9 +42,10 @@ const BLOCK_TYPES = [
 
 const VARIABILI = [
   '{{nome}}','{{cognome}}','{{ragione_sociale}}','{{email}}',
+  '{{cellulare}}','{{partita_iva}}','{{cap}}',
   '{{nome_evento}}','{{data_evento}}','{{luogo_evento}}',
   '{{qr_code}}','{{link_landing}}','{{link_questionario}}',
-  '{{data_iscrizione}}','{{numero_posto}}','{{link_conferma}}',
+  '{{data_iscrizione}}','{{codice_iscrizione}}','{{numero_posto}}','{{link_conferma}}',
 ]
 
 const PREVIEW_DATA = {
@@ -53,8 +54,10 @@ const PREVIEW_DATA = {
   '{{nome_evento}}':'Forum CNA Roma 2026',
   '{{data_evento}}':'Venerdi 25 settembre 2026, ore 09:30',
   '{{luogo_evento}}':'Palazzo dei Congressi, Roma',
+  '{{cellulare}}':'333 1234567','{{partita_iva}}':'12345670015','{{cap}}':'00100',
   '{{qr_code}}':'QR-MARCO2026','{{link_landing}}':'#','{{link_questionario}}':'#',
   '{{data_iscrizione}}': new Date().toLocaleDateString('it-IT'),
+  '{{codice_iscrizione}}':'EVT-2026-M1B2',
   '{{numero_posto}}':'Platea 14A','{{link_conferma}}':'#',
 }
 
